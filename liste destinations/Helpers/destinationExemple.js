45dd13be5f1cca08757c823de28d067045a9a293
StardustRL <73218046+StardustRL@users.noreply.github.com>
2021-03-24 16:31:51 +0100

    Add files via upload

---
+++ liste destinations/Helpers/destinationExemple.js
@@ -0,0 +1,56 @@
+export default data=[
+  {
+    id:47659231,
+    latitude:"59° 56' 02",
+    longitude:"59° 65'",
+    note:5,
+    nom_voyage:"Marly-Gaumont",
+    image_path:"",
+    description:"Une petite ville ma foi fort sympathique avec du silence."
+  },
+  {
+    id:32424322,
+    latitude:"23° 6' 342",
+    longitude:"59° 65 dede",
+    note:0,
+    nom_voyage:"J'ai pas d'idée",
+    image_path:"",
+    description: "Un endroit quelque part paisible et sans trace humaine"
+  },
+  {
+    id:32342879862,
+    latitude:"23° 6' 342",
+    longitude:"59° 65 dede",
+    note:2,
+    nom_voyage:"Paris",
+    image_path:"",
+    description: "Ville avec BEAUCOUP de pigeons."
+  },
+  {
+    id:32342879862,
+    latitude:"23° 6' 342",
+    longitude:"59° 65 dede",
+    note:2,
+    nom_voyage:"NOISNCAIN",
+    image_path:"",
+    description: "Village du sud de la France."
+  },
+  {
+    id:32342879862,
+    latitude:"23° 6' 342",
+    longitude:"59° 65 dede",
+    note:2,
+    nom_voyage:"Toulouse",
+    image_path:"",
+    description: "A380."
+  },
+  {
+    id:32342879862,
+    latitude:"23° 6' 342",
+    longitude:"59° 65 dede",
+    note:2,
+    nom_voyage:"Bordeaux",
+    image_path:"",
+    description: "Ville sans Starbucks."
+  }
+]
